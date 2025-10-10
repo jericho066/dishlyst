4f553d86a8d48749f3c76e2ce41f5d0339268146
Jerico Oliver <oliverjerico96@gmail.com>
2025-10-10 17:09:00 +0800

    message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const REPO_NAME = 'dishlyst'
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGitHubPages ? `/${REPO_NAME}/` : '/',
   plugins: [react()],
 })
